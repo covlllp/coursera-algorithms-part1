22ae47541cdb25e4d2321273d030741eb5dbbf6c
Colin VanLang <colinvanlang@gmail.com>
2015-02-04 17:54:53 -0500

    small changes to progset3

--- progset3/graph_min_cuts.js
+++ progset3/graph_min_cuts.js
@@ -1,3 +1,29 @@
+
+var fs = require('fs');
+
+fs.readFile('kargerMinCut.txt', function(err, data) {
+	// var data = fs.readFileSync('kargerMinCut.txt');
+	if (err) throw(err);
+
+	var min_cuts = Infinity;
+	var times = 10;
+	for (var i = 0; i < Math.floor(times * Math.log(times)); i++) {
+		var nodeList = new NodeList(data.toString().split('\n').map(function(str) {
+			var str_arr = str.split('\t').map(function(val) {
+				return val * 1;
+			}).filter(function(val) {
+				return val > 0;
+			});
+			return new Node(str_arr.shift(), str_arr);
+		}));
+
+		var cuts = nodeList.findMinCut();
+		if (cuts < min_cuts) min_cuts = cuts;
+	};
+	console.log('min_cuts is ', min_cuts);
+});
+
+
 var Node = function(node, edges) {
 	this.name = node;
 	this.edges = edges;
@@ -26,19 +52,26 @@ var NodeList = function(arr) {
 	this.nodes = arr;
 }
 
-NodeList.prototype.collapseEdge = function(node1_name, node2_name) {
-	var node1_ind = this.indexOf(node1_name);
-	var node2_ind = this.indexOf(node2_name);
-	var node1 = this.nodes[node1_ind];
-	var node2 = this.nodes[node2_ind];
+NodeList.prototype.findMinCut = function() {
+	while (this.nodes.length > 2) {
+		var randNode_ind = Math.floor(Math.random() * this.nodes.length);
+		var randNode = this.nodes[randNode_ind];
+		var randEdge_ind = Math.floor(Math.random() * randNode.edges.length);
+		var randEdge = randNode.edges[randEdge_ind];
 
-	// check if nodes are connected
-	if (node1.getIndexOfEdge(node2_name) == -1) return;
+		this.collapseEdge(randNode, randEdge);
+	}
+	return this.nodes[0].edges.length;
+};
+
+NodeList.prototype.collapseEdge = function(node1, node2_name) {
+	var node2_ind = this.indexOf(node2_name);
+	var node2 = this.nodes[node2_ind];
 
 	// Combine node edges and remove all self loops
 	node1.combineEdgesWith(node2);
 	this.nodes.splice(node2_ind, 1);
-	this.updateEdgesFromTo(node2_name, node1_name);
+	this.updateEdgesFromTo(node2_name, node1.name);
 };
 
 NodeList.prototype.indexOf = function(node_name) {
@@ -60,37 +93,3 @@ NodeList.prototype.updateEdgesFromTo = function(from_node_name, to_node_name) {
 		}
 	});
 };
-
-var fs = require('fs');
-
-fs.readFile('kargerMinCut.txt', function(err, data) {
-	// var data = fs.readFileSync('kargerMinCut.txt');
-	if (err) throw(err);
-
-	var min_cuts = Infinity;
-	var times = 200;
-	for (var i = 0; i < Math.floor(times * Math.log(times)); i++) {
-		// console.log('i is ', i, ', min_cuts is ', min_cuts);
-		var nodeList = new NodeList(data.toString().split('\n').map(function(str) {
-			var str_arr = str.split('\t').map(function(val) {
-				return val * 1;
-			}).filter(function(val) {
-				return val > 0;
-			});
-			return new Node(str_arr.shift(), str_arr);
-		}));
-
-		while (nodeList.nodes.length > 2) {
-			var randNode_ind = Math.floor(Math.random() * nodeList.nodes.length);
-			var randNode = nodeList.nodes[randNode_ind];
-			var randEdge_ind = Math.floor(Math.random() * randNode.edges.length);
-			var randEdge = randNode.edges[randEdge_ind];
-
-			nodeList.collapseEdge(randNode.name, randEdge);
-		}
-
-		var cuts = nodeList.nodes[0].edges.length;
-		if (cuts < min_cuts) min_cuts = cuts;
-	};
-	console.log('min_cuts is ', min_cuts);
-});
