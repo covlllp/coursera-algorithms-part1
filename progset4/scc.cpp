a6f2ccebb51179de82fbd7db9a0ef400047c2b84
Colin VanLang <colinvanlang@gmail.com>
2015-02-12 14:20:48 -0500

    dfs working

--- progset4/scc.cpp
+++ progset4/scc.cpp
@@ -3,39 +3,63 @@
 #include <string>
 #include <vector>
 #include <stack>
+// #include <algorithm>
 using namespace std;
 
 struct Node {
 	string name;
 	int seen, f;
 	vector<Node*> edges, reverseEdges;
-	Node(string n) : name(n) {}
+	Node(string n){
+		name = n;
+		seen = 0;
+	}
 };
 
 struct Graph {
 	vector<Node*> nodes;	// size is always one more; 0 index not used
+	vector<int> scc;		// contains all scc sizes
 };
 
+void printVector(vector<int> vec);
 void addEdge(Node &node, Node* node_name);
 void addReverseEdge(Node &node, Node* node_name);
 void addNodeToGraph(Graph &graph, Node* pt, int ind);
 void addEdgeToNodeOnGraph(Graph &graph, string node_name, string edge_name);
 void unseeNodes(Graph &graph);
 Graph* populate(string fileName);
-void dfs_loop(Graph* graph, string direction);
-void dfs(Node* node, string direction, int &t, int &s);
+Graph* dfs_loop(Graph* graph, string direction);
+void dfs(Node* node, string direction, int &t, int &s, Graph* graph);
+bool compare(int a, int b);
 
 
 
 int main()
 {
 	cout << "Start populating" << endl;
-	Graph* graph_pt = populate("SCC.txt");
+	Graph* graph = populate("test.txt");
 	cout << "Done populating, starting first dfs loop" << endl;
 
 	cout << "Start DFS loop on reverse graph" << endl;
-	dfs_loop(graph_pt, "reverse");
+	graph = dfs_loop(graph, "reverse");
 	cout << "DFS reverse loop complete" << endl;
+
+	for (int i = 1; i < graph->nodes.size(); i++) {
+		cout << "Node " << graph->nodes[i]->name << " found at " << i << endl;
+	}
+
+	// cout << "Unsee all nodes" << endl;
+	// unseeNodes(*graph);
+	// cout << "Unseeing nodes complete" << endl;
+
+	// cout << "Start DFS loop on forward graph" << endl;
+	// graph = dfs_loop(graph, "forward");
+	// cout << "DFS forward loop complete" << endl;
+
+	// cout << "Print out SCCs" << endl;
+	// printVector(graph->scc);
+
+
 	// cout << graph_pt->nodes.size() << endl;
 	// cout << graph_pt->nodes[3]->seen << endl;
 
@@ -45,21 +69,24 @@ int main()
 }
 
 
-
+void printVector(vector<int> vec) {
+	// print current graph.nodes
+	cout << "size of vector is " << vec.size() << endl;
+	for (int i = 0; i < vec.size(); i++) {
+		cout << " " << vec[i];
+	}
+}
 
 void addEdge(Node &node, Node* node_name) {
-	node.edges.resize(node.edges.size() + 1);
 	node.edges.push_back(node_name);
 }
 
 void addReverseEdge(Node &node, Node* node_name) {
-	node.reverseEdges.resize(node.reverseEdges.size() + 1);
 	node.reverseEdges.push_back(node_name);
 }
 
 void addNodeToGraph(Graph &graph, Node* pt, int ind) {
 	graph.nodes[ind] = pt;
-	// graph.nodes.insert(graph.nodes.begin() + ind, pt);
 }
 
 void addEdgeToNodeOnGraph(Graph &graph, string node_name, string edge_name) {
@@ -80,25 +107,12 @@ void addEdgeToNodeOnGraph(Graph &graph, string node_name, string edge_name) {
 		addNodeToGraph(graph, edge, edge_name_int);
 	}
 
-	// // print current graph.nodes
-	// cout << "size of vector is " << graph.nodes.size();
-	// cout << ", max_ind is " << max_ind;
-	// cout << ", node_name is " << node_name;
-	// cout << ", edge_name is " << edge_name << endl;
-	// for (int i = 0; i < graph.nodes.size(); i++) {
-	// 	cout << "index " << i << ": " << graph.nodes[i] << endl;
-	// }
-
-
-	Node* node_pt = graph.nodes[node_name_int];
-	Node node = *node_pt;
-
-	// Node node = *graph.nodes[node_name_int];
-	Node edge = *graph.nodes[edge_name_int];
+	Node* node = graph.nodes[node_name_int];
+	Node* edge = graph.nodes[edge_name_int];
 
 	// add front and reverse edge
-	addEdge(node, &edge);
-	addReverseEdge(edge, &node);
+	addEdge(*node, edge);
+	addReverseEdge(*edge, node);
 }
 
 void unseeNodes(Graph &graph) {
@@ -127,48 +141,60 @@ Graph* populate(string fileName) {
 	return graph_pt;
 }
 
-void dfs_loop(Graph* graph, string direction) {
+Graph* dfs_loop(Graph* graph, string direction) {
+	Graph* new_graph = new Graph();
+	int size = graph->nodes.size() - 1;
+	new_graph->nodes.resize(size);
 	int t = 0;
 	int s;
-	vector<int> s_container;
 	for (int i = graph->nodes.size() - 1; i; i--) {
 		Node* node = graph->nodes[i];
 		if (!node->seen) {
 			s = 0;
-			dfs(node, direction, t, s);
-			s_container.push_back(s);
+			dfs(node, direction, t, s, new_graph);
+			new_graph->scc.push_back(s);
 		}
 	}
+	// sort(new_graph->nodes.begin(), new_graph->nodes.end(), compare);
+	return new_graph;
 }
 
-void dfs(Node* start_node, string direction, int &t, int &s) {
-	stack<Node*> mystack;
-	mystack.push(start_node);
+void dfs(Node* start_node, string direction, int &t, int &s, Graph* graph) {
+	vector<Node*> mystack;
+	mystack.push_back(start_node);
 
 	while (mystack.size()) {
-		Node* node = mystack.top();
-		mystack.pop();
+		Node* node = mystack.back();
+		mystack.pop_back();
 		if (node->seen == 0) {
 			s++;
 			node->seen++;
-			mystack.push(node);
+			mystack.push_back(node);
 			if (direction == "forward") {
 				for (int i = 0; i < node->edges.size(); i++) {
 					Node* edge = node->edges[i];
-					if (edge->seen == 0)
-						mystack.push(edge);
+					if (edge->seen == 0) {
+						mystack.push_back(edge);
+					}
 				}
 			} else if (direction == "reverse") {
 				for (int i = 0; i < node->reverseEdges.size(); i++) {
 					Node* edge = node->reverseEdges[i];
-					if (edge->seen == 0)
-						mystack.push(edge);
+					if (edge->seen == 0) {
+						mystack.push_back(edge);
+					}
 				}
 			}
 		} else if (node->seen == 1) {
 			node->seen++;
 			t++;
 			node->f = t;
+			addNodeToGraph(*graph, node, t);
 		}
+		node = NULL;
 	}
+}
+
+bool compare(int a, int b) {
+	return (a > b);
 }
